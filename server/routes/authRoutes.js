5d8eea3dafbf5b3f52af5002847f242c20e103ba
DinAstral <dinmarcalan@gmail.com>
2024-09-09 10:58:00 +0800

    Heroku deploy

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -74,6 +74,17 @@ router.use(
   })
 );
 
+router.get("/*", function (req, res) {
+  res.sendFile(
+    path.join(__dirname, "../Client/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
+
 //Mobile route
 router.post("/mobileLogin", mobileLogin); //okay
 router.get("/getStudent/:LRN", getStudentbyLRN);
