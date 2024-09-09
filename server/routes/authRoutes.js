456722b0ddd43a3b6f4e91c37da15ba948c39778
DinAstral <dinmarcalan@gmail.com>
2024-09-09 19:38:55 +0800

    Error: URL

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -67,7 +67,7 @@ const { compareAudio } = require("../controllers/CompareController");
 // Configure CORS middleware
 router.use(
   cors({
-    origin: process.env.CLIENT_URL, // Update this with your client's URL
+    origin: "https://testing-baybay-salita-2c03b1d9b5a4.herokuapp.com", // Update this with your client's URL
     methods: ["GET", "POST", "DELETE", "PATCH"], // Add the allowed HTTP methods
     credentials: true, // Allow credentials (cookies, authorization headers)
   })
