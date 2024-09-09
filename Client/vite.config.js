1465058f48e4b88d8addcd0931d8c882f5e39f60
DinAstral <dinmarcalan@gmail.com>
2024-09-09 12:49:32 +0800

    heroku: Ito na talaga I can feel it

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React-Heruku-BaybaySalita/",
+  base: "/",
   server: {
     host: true, // use specific IP address
     port: 3000,
